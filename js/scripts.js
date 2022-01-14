c752643a206e16fb14d783ece6db883c814f02b7
Will Greenberg <greenberg.will@gmail.com>
2022-01-14 09:10:33 -0800

    add name, email, phone number form

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,8 @@
 $(document).ready(function(){
   $("#form").submit(function(event){
     event.preventDefault();
-    
+    $("#results").hide();
   });
 });
+
+// maybe try the function Math.max(put parameters in here) to find the value with highest number of matches
